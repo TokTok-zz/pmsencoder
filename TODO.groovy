600f48d0e577b6366bf3e6f7e2159abd56db42c5
chocolateboy <chocolate@cpan.org>
2011-03-31 02:35:02 +0100

    rm TODONE

--- TODO.groovy
+++ TODO.groovy
@@ -1,63 +1,22 @@
 // move all of this to GitHub issues
 
+// rename PMSMonkey
+
+// add support for audio
+
+// investigate adding support for images
+
 /* add $mencoder_feature_level (bool) to stash based on MEncoder version */
 
-/* XPath scraping? */
-
-    scrape(
-        uri:    uri, // default
-        xpath:  '//foo/bar/@baz',
-        regex:  'foo:(?<bar>bar):baz'
-        format: 'html' // default if xpath is defined
-    )
-
-/*
-    document the outstanding issue with e.g. "${URI}&has_verified" barfing:
-
-        java.net.URISyntaxException: Illegal character in path at index 5: class java.net.URI&has_verified=1
-
-    investigate using a custom GroovyShell without URI autoimported
-
-*/
-
-/*
-    infinite loop/stack overflow in maven assembly plugin (in Plexus Archiver) with
-    Groovy++ 0.2.26: https://groups.google.com/group/groovyplusplus/msg/a765fe77975650db
-*/
+// get rid of sigils and upper case names
 
 // script management: disable/enable scripts through the Swing UI (cf. GreaseMonkey)
 
 /*
-    fix the sigil mess - the whole thing is a workaround for the URI property conflicting with the class
-    groovysh has the same problem, but groovy script.groovy doesn't
-    also: https://code.google.com/p/awsgroovyclouds/source/browse/trunk/AWSDrivers/src/com/groovyclouds/aws/S3Driver.groovy#897
-
-        private static final def URI = "URI"
-*/
-
-/*
-  investigate adding seek support for YouTube videos:
+  investigate adding seek support for YouTube videos (e.g. YouTubeEngine):
 
       http://stackoverflow.com/questions/3302384/youtubes-hd-video-streaming-server-technology
 */
-
-// WEB.groovy (path is relative to the renderer's root folder):
-
-videostream ('Web/TV') {
-    uri  = 'mms://example.com/stream'
-    name = 'Example'
-    thumbnail = 'http://example.com/rss.jpg' // optional
-}
-
-// use the user-specified folder, rather than appending the feed name, so feeds can be merged:
-
-videofeed ('Web/YouTube/Favourites') {
-    uri = 'http://youtube.com/api/whatever?1-50'
-}
-
-videofeed ('Web/YouTube/Favourites') {
-    uri = 'http://youtube.com/api/whatever?50-100'
-}
 
 /*
 
@@ -81,16 +40,16 @@ Should be:
 Read-only options could go in request and r/w objects in response e.g.
 
 new Request(
-    DOWNLOADER_OUT: ...,
-    TRANSCODER_OUT: ...,
-    URI: ...,
+    downloader_out: ...,
+    transcoder_out: ...,
+    uri: ...,
 )
 
 new Response(
-    ARGS: ...,
-    DOWNLOADER: ...
-    TRANSCODER: ...
-    URI: ...
+    args: ...,
+    downloader: ...
+    transcoder: ...
+    uri: ...
 )
 
 matcher.run(request, response)
@@ -99,15 +58,6 @@ http://stackoverflow.com/questions/325346/name-for-http-requestresponse
 http://stackoverflow.com/questions/1039513/what-is-a-request-response-pair-called
 
 */
-
-// Fix the Script delegate so that globals can be shared e.g. (unix_paths_example.groovy):
-
-script {
-    PERL             = '/usr/bin/perl'
-    PYTHON           = '/usr/bin/python'
-    YOUTUBE_DL       = '/usr/bin/youtube-dl'
-    GET_FLASH_VIDEOS = '/usr/bin/get-flash-videos'
-}
 
 // make $URI a URI rather than a String?
 
@@ -129,31 +79,11 @@ script {
     scrape { 'foo/bar(?<baz>\\w+)' }
     scrape (uri: uri) { 'foo/bar(?<baz>\\w+)' }
 
-// download/install standard library (GitHub API/JGit):
-
-    pmsencoder.library.root = /tmp/pmsencoder/scripts
-    pmsencoder.library.repository = http://example.com/path/to/scripts,http://...
-
-// asynchronously downloads + installs scripts for this version of PMSEncoder and saves them to the specified
-// (writable) location under the version
-
-    /tmp/pmsencoder/scripts/1.3.0
-    /tmp/pmsencoder/scripts/1.4.0
-
-// Do this by default and eliminate the built-in DEFAULT.groovy?
-
-// query youtube-dl and get-flash-videos for supported sites at startup?
-
-// add (overridable) INIT.groovy (or DEFAULT.groovy) for one-off initializations (e.g. $DEFAULT_MENCODER_ARGS)
-
-// print a debug version of the MEncoder (if used) command-line (i.e. pump up the debug level), target e.g.
-// deleteme.tmp, and quote the URI (i.e. would need to be done in DEFAULT.groovy)
-
-// add namespace support (required?):
+// script metadata?
 
     script (namespace: 'http://www.example.com', author: 'chocolateboy', version: 1.04) { ... }
 
-// use a web interface because a) Swing sucks and b) headless servers. Only use swing to enable/disable the web server
+// use a web interface because a) Swing sucks and b) headless servers. Only use Swing to enable/disable the web server
 // and set the port.
 
 // investigate using busybox-w32/ash instead of cmd.exe on Windows
@@ -167,32 +97,17 @@ script {
 
 // use URI for protocol parsing rather than a regex
 
-/*
+// make action methods (e.g. set, append &c.) methods on the downloader/transcoder e.g.
 
-Groovy++ bytecode compilation error (both at compile-time and runtime): see Plugin.groovy
-
-[ERROR] Failed to execute goal org.codehaus.gmaven:gmaven-plugin:1.3:compile (default) on project pmsencoder: startup failed:
-[ERROR] /home/chocolateboy/dev/public/pmsencoder/src/main/groovy/com/chocolatey/pmsencoder/Plugin.groovy: 50: Internal Error: java.lang.VerifyError: (class: com/chocolatey/pmsencoder/Plugin, method: <init> signature: ()V) Register 3 contains wrong type
-[ERROR] @ line 50, column 5.
-[ERROR] public Plugin() {
-[ERROR] ^
-[ERROR] org.codehaus.groovy.syntax.SyntaxException: Internal Error: java.lang.VerifyError: (class: com/chocolatey/pmsencoder/Plugin, method: <init> signature: ()V) Register 3 contains wrong type
-[ERROR] @ line 50, column 5.
-[ERROR] at org.mbte.groovypp.compiler.CompilerTransformer.addError(CompilerTransformer.java:92)
-[ERROR] at org.mbte.groovypp.compiler.StaticMethodBytecode.<init>(StaticMethodBytecode.java:84)
-[ERROR] at org.mbte.groovypp.compiler.StaticMethodBytecode.replaceMethodCode(StaticMethodBytecode.java:98)
-[ERROR] at org.mbte.groovypp.compiler.CompileASTTransform.visit(CompileASTTransform.java:108)
-[ERROR] at org.codehaus.groovy.transform.ASTTransformationVisitor.visitClass(ASTTransformationVisitor.java:129)
-[ERROR] at org.codehaus.groovy.transform.ASTTransformationVisitor$2.call(ASTTransformationVisitor.java:172)
-[ERROR] at org.codehaus.groovy.control.CompilationUnit.applyToPrimaryClassNodes(CompilationUnit.java:936)
-[ERROR] at org.codehaus.groovy.control.CompilationUnit.doPhaseOperation(CompilationUnit.java:513)
-[ERROR] at org.codehaus.groovy.control.CompilationUnit.processPhaseOperations(CompilationUnit.java:491)
-[ERROR] at org.codehaus.groovy.control.CompilationUnit.compile(CompilationUnit.java:468)
-[ERROR] at org.codehaus.groovy.control.CompilationUnit.compile(CompilationUnit.java:447)
-
-*/
+    def mplayer = new MPlayer()
+    transcoder.downloader = mplayer
+    downloader.set('-foo')
+    transcoder.remove('-bar')
 
 /*
+
+document this:
+
     script loading order:
 
         builtin scripts
@@ -206,23 +121,18 @@ Groovy++ bytecode compilation error (both at compile-time and runtime): see Plug
         check
         end
 
-document this:
-
     replacing a profile with a profile with a different name
     does not change its canonical name. this ensures other replacement profiles
     have a predictable, consistent name to refer to
-
-TODO: determine behaviour (if any) if a replacement has a different stage
-
-TODO: re-stage all the profiles in a script block, preserving the natural order
-
-keep a list of Script objects rather than (just) a hash of profiles?
-
 */
 
-// remove rtmpdump protocol and manage everything through pmsencoder://
+/*
+    TODO: determine behaviour (if any) if a replacement has a different stage
 
-// PMS.setValue('com.chocolatey.pmsencoder.Matcher', Matcher)
+    TODO: re-stage all the profiles in a script block, preserving the natural order
+
+    keep a list of Script objects rather than (just) a hash of profiles?
+*/
 
 // cleaner Gradle-style names (no sigils!):
 
@@ -246,7 +156,6 @@ profile('Foo') {
         // add a downloader
         def mplayer = new MPlayer(uri)
         mplayer.args = [ 'string', '-or', '-list' ]
-        mplayer.args.set([ '-foo': 'bar' ])
         transcoder.downloader = mplayer
     }
 }
@@ -274,17 +183,15 @@ reject $URI: '^concat:'
 
 /*
 
-Players:
+Transcoders:
 
     MEncoder
     FFmpeg
 
 Downloaders:
 
-    SopCast
     MPlayer
-    GetFlashVideos
-    YoutubeDL
+    CustomDownloader // e.g. get-flash-videos, youtube-dl
 */
 
 // Ruby-style initialization blocks?
@@ -322,11 +229,11 @@ transcoder = new NullTranscoder()
 
 /*
     var stage = new Stage('script');
-    var profile = new Profile('Foo');
+    var profile = new Profile('Foo', [ 'extends': 'Bar', 'replaces': 'Baz' ]);
     stage.addProfile(profile);
 
-    profile.pattern = function() {
-        if (url.match(/^http:\/\/www.whatever.com/) {
+    profile.pattern = function(context, uri) {
+        if (uri.match(/^http:\/\/www.whatever.com/) {
             return true;
         }
         return false
@@ -351,7 +258,7 @@ transcoder = new NullTranscoder()
 
 // propertyMissing + methodMissing?
 
-    uri = query { $(...).foo().bar().baz() }
+    uri = jQuery { $(...).foo().bar().baz() }
 
 // complement (asynchronous) $HOOK with $BEFORE and $AFTER. $AFTER attaches a dummy process started by stopProcess()
 
